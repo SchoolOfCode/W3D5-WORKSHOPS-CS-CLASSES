7899580bf116e56200c6480f416e68fc6de7393e
Roxana Tot <roxana.tot@gmail.com>
2021-01-22 11:59:24 +0000

    Task 1

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,24 @@
+using System;
+public class BankAccount
+{
+    long Id;
+    string Owner;
+    decimal Balance;
+    public BankAccount(long id, string owner, decimal balance)
+    {
+        Id = id;
+        Owner = owner;
+        Balance = balance;
+    }
+    public void MakeDeposit(decimal savings)
+    {
+        Balance += savings;
+        Console.WriteLine($"You have deposited {savings}, your balance is now {Balance}");
+    }
+    public decimal MakeWithdrawal(decimal withdrawal)
+    {
+        Balance -= withdrawal;
+        Console.WriteLine($"You have withdrawn {withdrawal}, your balance is now {Balance}");
+        return Balance;
+    }
+}
